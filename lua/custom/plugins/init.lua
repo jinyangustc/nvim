c4721ede2a1e95db50c60f1f6e5eadfbae125334
Jinyang Li <jinyang7@illinois.edu>
2024-07-15 15:33:47 -0500

    Prefer light theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,25 +3,25 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'rose-pine-dawn'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
   {
-    'bluz71/vim-moonfly-colors',
-    name = 'moonfly',
-    lazy = false,
+    'rose-pine/neovim',
+    name = 'rose-pine',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'moonfly'
+      vim.cmd.colorscheme 'rose-pine-dawn'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- {
+  --   'bluz71/vim-moonfly-colors',
+  --   name = 'moonfly',
+  --   lazy = false,
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'moonfly'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
     'tpope/vim-fugitive',
     config = function()
