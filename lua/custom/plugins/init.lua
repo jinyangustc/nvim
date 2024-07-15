de1362591b41f6fb6ddd3afe934207cca6d650e1
Jinyang Li <jinyang7@illinois.edu>
2024-07-15 17:40:23 -0500

    Try

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,16 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+--
+
 return {
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'rose-pine-dawn'
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'rose-pine-dawn'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   -- {
   --   'bluz71/vim-moonfly-colors',
   --   name = 'moonfly',
@@ -22,6 +24,18 @@ return {
   --     vim.cmd.hi 'Comment gui=none'
   --   end,
   -- },
+  -- { 'rktjmp/lush.nvim' },
+  {
+    'zenbones-theme/zenbones.nvim',
+    dependencies = { 'rktjmp/lush.nvim' },
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.zenbones = { lightness = 'dim', darken_comments = 45 }
+      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'zenbones'
+    end,
+  },
   {
     'tpope/vim-fugitive',
     config = function()
