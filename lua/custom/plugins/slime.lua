8c9e6345734dd585f20947e9f0fb8d8160f46c02
Jinyang Li <jinyang7@illinois.edu>
2024-07-16 16:58:04 -0500

    Move python setting to ftplugin

--- lua/custom/plugins/slime.lua
+++ lua/custom/plugins/slime.lua
@@ -8,7 +8,6 @@ return {
     },
     config = function()
       vim.g.slime_target = 'tmux'
-      vim.g.slime_cell_delimiter = '# %%'
       vim.g.slime_bracketed_paste = 1
       vim.g.slime_default_config = { socket_name = 'default', target_pane = '1' }
     end,
