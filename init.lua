3ca59cb593a10b29ebe748c8da590a391217d5bc
Jinyang Li <jinyang7@illinois.edu>
2024-07-18 16:24:51 -0500

    Inline error messages get truncated all the time; disable them

--- init.lua
+++ init.lua
@@ -477,6 +477,19 @@ require('lazy').setup({
       { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
+      vim.diagnostic.config {
+        underline = true,
+        signs = true,
+        virtual_text = false,
+        float = {
+          show_header = true,
+          source = 'if_many',
+          border = 'rounded',
+          focusable = false,
+        },
+        update_in_insert = false, -- default to false
+        severity_sort = false, -- default to false
+      }
       -- Brief aside: **What is LSP?**
       --
       -- LSP is an initialism you've probably heard, but might not understand what it is.
