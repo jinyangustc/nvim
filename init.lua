e0063da74455dd2ebd017cfd25957926669c723f
Jinyang Li <jinyang7@illinois.edu>
2024-07-22 18:17:57 -0500

    Replace `s` keymap with `f`

--- init.lua
+++ init.lua
@@ -338,7 +338,7 @@ require('lazy').setup({
           { '<leader>c', group = 'Code' },
           { '<leader>d', group = 'Document' },
           { '<leader>r', group = 'Refactor/Slime' },
-          { '<leader>s', group = 'Search' },
+          { '<leader>f', group = 'Find' },
           { '<leader>w', group = 'Workspace' },
           { '<leader>t', group = 'Toggle' },
           { '<leader>g', group = 'Git' },
@@ -405,11 +405,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+          layout_strategy = 'vertical',
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -424,15 +425,15 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Help' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Keymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Files' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Select Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Current Word' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Grep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Diagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Resume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help' })
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Keymaps' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Files' })
+      vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = 'Select Telescope' })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Current Word' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep' })
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Diagnostics' })
+      vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Resume' })
+      vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -446,7 +447,7 @@ require('lazy').setup({
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
+      vim.keymap.set('n', '<leader>f/', function()
         builtin.live_grep {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
@@ -454,7 +455,7 @@ require('lazy').setup({
       end, { desc = 'Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
+      vim.keymap.set('n', '<leader>fn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = 'Neovim files' })
     end,
@@ -720,7 +721,7 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>d=',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
