68a89f63af0ee46310d31a2067e2b186d788f585
Jinyang Li <jinyang7@illinois.edu>
2024-10-02 10:24:20 -0500

    Increase formatter timeout for `prettier`

--- init.lua
+++ init.lua
@@ -696,7 +696,7 @@ require('lazy').setup({
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true, json = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 2500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
