699c547a5201422ca164a2bc2b235cfe0c373eee
Jinyang Li <jinyang7@illinois.edu>
2024-10-04 01:29:36 -0500

    Use location list

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -3,8 +3,8 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git status' })
-      vim.keymap.set('n', '<leader>gh', '<cmd>0Gclog<cr>', { desc = 'File history' })
-      vim.keymap.set('n', '<leader>gl', '<cmd>Gclog<cr>', { desc = 'Git log' })
+      vim.keymap.set('n', '<leader>gh', '<cmd>0Gllog<cr>', { desc = 'File history' })
+      vim.keymap.set('n', '<leader>gl', '<cmd>Gllog<cr>', { desc = 'Git log' })
     end,
   },
   { 'tpope/vim-rhubarb' },
