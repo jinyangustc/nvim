3271e9f116fb79518fdd14ae4b51465ac9043009
Jinyang Li <jinyang7@illinois.edu>
2024-10-04 01:11:02 -0500

    Shortcuts

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -3,6 +3,8 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git status' })
+      vim.keymap.set('n', '<leader>gh', '<cmd>0Gclog<cr>', { desc = 'File history' })
+      vim.keymap.set('n', '<leader>gl', '<cmd>Gclog<cr>', { desc = 'Git log' })
     end,
   },
   { 'tpope/vim-rhubarb' },
