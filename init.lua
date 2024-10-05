16289fcf1ac98c6991596daf19bd4c36dceace87
Jinyang Li <jinyang7@illinois.edu>
2024-10-04 20:56:19 -0500

    Fix telescope for hidden files

--- init.lua
+++ init.lua
@@ -345,7 +345,14 @@ require('lazy').setup({
           },
           layout_strategy = 'vertical',
         },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
+            layout_config = {
+              height = 0.70,
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
