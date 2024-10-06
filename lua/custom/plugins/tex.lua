acfa9e57a439b398a46562d641ad18cab6a12943
Jinyang Li <jinyang7@illinois.edu>
2024-10-06 02:26:25 -0500

    Add vimtex

---
+++ lua/custom/plugins/tex.lua
@@ -0,0 +1,12 @@
+-- Getting started with the VimTeX plugin:
+-- https://ejmastnak.com/tutorials/vim-latex/vimtex/
+
+return {
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    init = function()
+      vim.g.vimtex_view_method = 'sioyek'
+    end,
+  },
+}
