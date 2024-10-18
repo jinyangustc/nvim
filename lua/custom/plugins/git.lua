ec31bb32d2c176361b1a2e4092f9ae4d9c4c3f91
Jinyang Li <jinyang7@illinois.edu>
2024-10-18 01:37:13 -0500

    Key binds for merging conflict

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,6 +5,9 @@ return {
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git status' })
       vim.keymap.set('n', '<leader>gh', '<cmd>0Gllog<cr>', { desc = 'File history' })
       vim.keymap.set('n', '<leader>gl', '<cmd>Gllog<cr>', { desc = 'Git log' })
+
+      vim.keymap.set('n', 'gh', '<cmd>diffget //2<cr>', { desc = 'Grab changes on the left' })
+      vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>', { desc = 'Grab changes on the right' })
     end,
   },
   { 'tpope/vim-rhubarb' },
