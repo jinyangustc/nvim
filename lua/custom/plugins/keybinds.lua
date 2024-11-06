9e445f8f325f65faffe3282b7a6b46976f61ce09
Jinyang Li <jinyang7@illinois.edu>
2024-11-05 22:08:13 -0600

    Toggle scrollbind

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>tn', '<cmd>set number!<cr>', { desc = 'Toggle Line Number' })
+vim.keymap.set('n', '<leader>ts', '<cmd>windo set scrollbind!<cr>', { desc = 'Toggle scrollbind' })
 return {}
