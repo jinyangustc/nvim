2e8e320c184fe375fa658c1cb8abb21e55107d36
Jinyang Li <jinyang7@illinois.edu>
2026-02-24 14:46:37 -0600

    Consolidate config as blink dependency

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -1,13 +1,12 @@
 return {
   {
     'L3MON4D3/LuaSnip',
+    version = '2.*',
     build = (function()
       -- Build Step is needed for regex support in snippets.
       -- This step is not supported in many windows environments.
       -- Remove the below condition to re-enable on windows.
-      if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-        return
-      end
+      if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then return end
       return 'make install_jsregexp'
     end)(),
     dependencies = {
@@ -21,6 +20,7 @@ return {
       --   end,
       -- },
     },
+    opts = {},
     config = function()
       local ls = require 'luasnip.loaders.from_lua'
       ls.lazy_load { paths = { '~/.config/nvim/snippets' } }
