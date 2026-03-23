e2bb46e8ba99cd9d93e514e0c0e762e8e6ad2c32
Jinyang Li <jinyang7@illinois.edu>
2026-03-23 13:48:21 -0500

    Add line wrapping toggle

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,3 +1,4 @@
-vim.keymap.set('n', '<leader>tn', '<cmd>set number!<cr>', { desc = 'Toggle Line Number' })
-vim.keymap.set('n', '<leader>ts', '<cmd>windo set scrollbind!<cr>', { desc = 'Toggle scrollbind' })
+vim.keymap.set('n', '<leader>tn', '<cmd>set number!<cr>', { desc = '[T]oggle Line [N]umber' })
+vim.keymap.set('n', '<leader>tw', '<cmd>set wrap!<cr>', { desc = '[T]oggle Soft [W]rap' })
+vim.keymap.set('n', '<leader>ts', '<cmd>windo set scrollbind!<cr>', { desc = '[T]oggle [s]crollbind' })
 return {}
