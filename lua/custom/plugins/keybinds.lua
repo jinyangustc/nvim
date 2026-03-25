9b1b82e0e9eabca2e2b3ea4b501dc730d58bfe2f
Jinyang Li <jinyang7@illinois.edu>
2026-03-25 15:01:06 -0500

    More friendly key binds to resize splits

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,4 +1,9 @@
 vim.keymap.set('n', '<leader>tn', '<cmd>set number!<cr>', { desc = '[T]oggle Line [N]umber' })
 vim.keymap.set('n', '<leader>tw', '<cmd>set wrap!<cr>', { desc = '[T]oggle Soft [W]rap' })
 vim.keymap.set('n', '<leader>ts', '<cmd>windo set scrollbind!<cr>', { desc = '[T]oggle [s]crollbind' })
+
+vim.keymap.set('n', '<S-Left>', '<cmd>vertical resize -3<CR>', { desc = 'Resize split left' })
+vim.keymap.set('n', '<S-Right>', '<cmd>vertical resize +3<CR>', { desc = 'Resize split right' })
+vim.keymap.set('n', '<S-Up>', '<cmd>resize +3<CR>', { desc = 'Resize split taller' })
+vim.keymap.set('n', '<S-Down>', '<cmd>resize -3<CR>', { desc = 'Resize split shorter' })
 return {}
