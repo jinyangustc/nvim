0e0bb1c1b49ae7c90a3e9cfbf200e03cedfa425e
Jinyang Li <jinyang7@illinois.edu>
2026-03-28 17:04:01 -0500

    Use <C-h> to toggle whether to show ignore/hidden files

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -9,4 +9,31 @@ vim.keymap.set('n', '<S-Right>', '<cmd>vertical resize +3<CR>', { desc = 'Resize
 vim.keymap.set('n', '<S-Up>', '<cmd>resize +3<CR>', { desc = 'Resize split taller' })
 vim.keymap.set('n', '<S-Down>', '<cmd>resize -3<CR>', { desc = 'Resize split shorter' })
 
+local my_find_files
+my_find_files = function(opts, no_ignore)
+  opts = opts or {}
+  no_ignore = vim.F.if_nil(no_ignore, false)
+  opts.attach_mappings = function(_, map)
+    map({ 'n', 'i' }, '<C-h>', function(prompt_bufnr) -- <C-h> to toggle modes
+      local prompt = require('telescope.actions.state').get_current_line()
+      require('telescope.actions').close(prompt_bufnr)
+      no_ignore = not no_ignore
+      my_find_files({ default_text = prompt }, no_ignore)
+    end)
+    return true
+  end
+
+  if no_ignore then
+    opts.no_ignore = true
+    opts.hidden = true
+    opts.prompt_title = 'Find Files <ALL>'
+    require('telescope.builtin').find_files(opts)
+  else
+    opts.prompt_title = 'Find Files'
+    require('telescope.builtin').find_files(opts)
+  end
+end
+
+vim.keymap.set('n', '<leader>sf', my_find_files, { desc = '[S]earch [A]ll files' }) -- you can then bind this to whatever you want
+
 return {}
