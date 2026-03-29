d5108f0873e9162cce4f5e693fc5181d66aa7623
Jinyang Li <jinyang7@illinois.edu>
2026-03-29 17:48:34 -0500

    .

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 if vim.g.neovide then
-  vim.o.guifont = 'Aporetic Sans Mono:h18'
+  vim.o.guifont = 'DepartureMono Nerd Font:h16'
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
 end
 
-if vim.g.gui_vimr then vim.o.guifont = 'Aporetic Sans Mono:h18' end
+if vim.g.gui_vimr then vim.o.guifont = 'DepartureMono Nerd Font:h16' end
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
